2dd1d729a9cedf4a4cd68bfcaf13ceb71245eeb7
feixiao <feixiao2020@sina.com>
2025-11-03 14:06:42 +0800

    other formats

--- ch1/py/i-csv.py
+++ ch1/py/i-csv.py
@@ -1,5 +1,7 @@
 from langchain_core.output_parsers import CommaSeparatedListOutputParser
 
+
+# You can also use an LLM or chat model to produce output in other formats, such as CSV or XML. 
 parser = CommaSeparatedListOutputParser()
 
 response = parser.invoke("apple, banana, cherry")
